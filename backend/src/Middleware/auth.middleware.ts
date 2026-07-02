9a4a6f80e3d6682880648692d8e613fbd898a7db
Baqir <bakaransari686@gmail.com>
2026-07-02 08:04:49 +0500

    Fix JWT_SECRET env var typo in auth middleware

--- backend/src/Middleware/auth.middleware.ts
+++ backend/src/Middleware/auth.middleware.ts
@@ -9,7 +9,7 @@ export const tokenVerify =async (req:Request,res:Response,next:NextFunction) : P
     if(!token)
         return res.status(401).json({message:"Token Missing"})
     try{
-    const verifyt = JWT.verify(token,process.env.JWT_SECREt!);
+    const verifyt = JWT.verify(token,process.env.JWT_SECRET!);
 
     (req as any).user_id = (verifyt as any).id;
     next();
