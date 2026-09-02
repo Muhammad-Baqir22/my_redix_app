e2847c50dc1477d75a3e5d3a40dae5b2576f7133
Baqir <bakaransari686@gmail.com>
2026-09-02 22:02:36 +0500

    Pass correct CORS options to OPTIONS handler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,7 +28,7 @@ app.use(cors({
   exposedHeaders: ['Authorization'],
   methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
 }));
-app.options(/.*/, cors());
+app.options(/.*/, cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 
 app.use('/api/users', userRouter);
